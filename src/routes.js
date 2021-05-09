a7496a09d784bd13abf5da06702d5e0022281015
frhnardi <aardifarhann@gmail.com>
2021-05-10 03:26:16 +0700

    fix bugs host value

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,36 @@
-import { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } from './handler';
-
+const {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+  } = require('./handler');
 const routes = [
-  {
-    method: 'POST',
-    path: '/notes',
-    handler: addNoteHandler,
-  },
-  {
-    method: 'GET',
-    path: '/notes',
-    handler: getAllNotesHandler,
-  },
-  {
-    method: 'GET',
-    path: '/notes/{id}',
-    handler: getNoteByIdHandler,
-  },
-  {
-    method: 'GET',
-    path: '/notes/{id}',
-    handler: editNoteByIdHandler,
-  },
-  {
-    method: 'DELETE',
-    path: '/notes/{id}',
-    handler: deleteNoteByIdHandler,
-  },
-];
-
-export default routes;
+    {
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+      },
+      {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+      },
+      {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+      },
+      {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+      },
+  ];
+   
+  module.exports = routes;
